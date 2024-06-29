056c0da0910f1a33572634a2a8eca298bf12d4ad
Alireza Sadeghi <alirezasadeghi2300@gmail.com>
2024-06-29 12:56:26 +0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace landing_project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
