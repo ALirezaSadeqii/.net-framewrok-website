abadc54d6569a3ec95f18a848f915994fefe1218
Alireza Sadeghi <alirezasadeghi2300@gmail.com>
2024-06-29 21:30:33 +0300

    responsive other than navbar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function toggleMenu() {
+  const menu = document.querySelector(".navbarr .menu");
+  menu.classList.toggle("active");
+}
